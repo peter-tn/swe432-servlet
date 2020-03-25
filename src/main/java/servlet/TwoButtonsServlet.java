2fd701fb7fe943c453006b28e0f93ee581b4c8fb
peter <peternguyentn98@gmail.com>
2020-03-25 14:13:38 -0400

    renamed servlet name

--- src/main/java/servlet/TwoButtonsServlet.java
+++ src/main/java/servlet/TwoButtonsServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "MyServlet",
+        name = "MyServlet2",
         urlPatterns = {"/twoButtons"}
     )
 
