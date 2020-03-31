8e4a22ecb56250d52e45c933bc62c0776d7a37b7
peter <peternguyentn98@gmail.com>
2020-03-30 23:02:56 -0400

    added 2nd simplified js function

--- src/main/java/servlet/RatingServlet.java
+++ src/main/java/servlet/RatingServlet.java
@@ -79,7 +79,8 @@ public class RatingServlet extends HttpServlet
       out.println(".row:after { content: ''; display: table; clear: both; }");
       out.println("</style>");
       out.println("<script type='text/javascript'>");
-      out.println("function checkForm() { if (document.form['1st picture rating'].value == '' || document.form['2nd picture rating'].value == '' || document.form['3rd picture rating'].value == '' || document.form['4th picture rating'].value == '') { alert('Please give all pictures a rating :)'); return false;} if (document.form.userExplanation.value == '') { document.getElementById('userExplanation').style.borderColor = 'red'; document.getElementById('userExplanation').style.borderWidth = '3px'; setTimeout(function() { alert('Please tell us how you rated :)'); }, 10) return false; } return true;}");
+      // out.println("function checkForm() { if (document.form['1st picture rating'].value == '' || document.form['2nd picture rating'].value == '' || document.form['3rd picture rating'].value == '' || document.form['4th picture rating'].value == '') { alert('Please give all pictures a rating :)'); return false;} }");
+      out.println("function checkForm() { if (document.form.userExplanation.value == '') { document.getElementById('userExplanation').style.borderColor = 'red'; document.getElementById('userExplanation').style.borderWidth = '3px'; setTimeout(function() { alert('Please tell us how you rated :)'); }, 10) return false; } return true;}");
       out.println("function resetText() { document.getElementById('userExplanation').style.borderColor=''; document.getElementById('userExplanation').style.borderWidth=''; }");
       out.println("</script>");
       out.println("</head>");
