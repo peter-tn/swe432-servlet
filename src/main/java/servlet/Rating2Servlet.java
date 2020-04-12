27268ee3bd5564a1c3908910f09ab2ed548c669c
peter <peternguyentn98@gmail.com>
2020-04-12 17:35:56 -0400

    added new servlet for react rating page

---
+++ src/main/java/servlet/Rating2Servlet.java
@@ -0,0 +1,45 @@
+package servlet;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "MyServlet4",
+    urlPatterns = {"/rating2"}
+)
+
+public class Rating2Servlet extends HttpServlet {
+
+    // Location of servlet.
+    static String Path = "swe432-assignment.herokuapp.com/rating2";
+
+   /** *****************************************************
+     *  Overrides HttpServlet's doPost().
+     *  Converts the values in the form, performs the operation
+     *  indicated by the submit button, and sends the results
+     *  back to the client.
+    ********************************************************* */
+    public void doPost (HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        out.print("{\"test\":\"5\"}");
+        out.flush();
+    }
+
+    /** *****************************************************
+     *  Overrides HttpServlet's doGet().
+     *  Prints an HTML page with a blank form.
+    ********************************************************* */
+    public void doGet (HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       
+    } // End doGet
+}
