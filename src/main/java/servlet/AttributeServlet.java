ac171bbdf5cc3258613012773fd91ced8b459b77
peter <peternguyentn98@gmail.com>
2020-04-21 18:54:19 -0400

    added action for invalidating session; added another space between invalidate session link and buttons below it

--- src/main/java/servlet/AttributeServlet.java
+++ src/main/java/servlet/AttributeServlet.java
@@ -22,11 +22,39 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    // Get session object
    HttpSession session = request.getSession();
 
+   String action = request.getParameter("action");
    String name   = request.getParameter("attrib_name");
    String value  = request.getParameter("attrib_value");
    String remove = request.getParameter("attrib_remove");
+   String url = response.encodeURL("sessionAttributes");
 
-   if (remove != null && remove.equals("on"))
+   if (action != null && action.equals("invalidate"))
+   {  // Called from the invalidate button, kill the session.
+      // Get session object
+      session.invalidate();
+
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+
+      out.println("<html>");
+      out.println("<head>");
+      out.println(" <title>Session lifecycle</title>");
+      out.println("</head>");
+      out.println("");
+      out.println("<body>");
+
+      out.println("<p>Your session has been invalidated.</P>");
+
+      // Create a link so the user can create a new session.
+      // The link will have a parameter builtin
+      out.println("<a href=\"" + url + ">");
+      out.println("Create new session</A>");
+
+      out.println("</body>");
+      out.println("</html>");
+      out.close();
+   }
+   else if (remove != null && remove.equals("on"))
    {
       session.removeAttribute(name);
    }
@@ -55,8 +83,6 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
 
    out.println("Enter name and value of an attribute");
 
-   // String url = response.encodeURL ("offutt/servlet/attributeServlet");
-   String url = response.encodeURL("sessionAttributes");
    out.println("<form action=\"" + url + "\" method=\"GET\">");
    out.println(" Name: ");
    out.println(" <input type=\"text\" size=\"10\" name=\"attrib_name\">");
@@ -68,7 +94,7 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    out.print  ("<br><br><a href=\"" + url + "?action=invalidate\">");
    out.println("Invalidate the session</a>");
 
-   out.println(" <br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
+   out.println(" <br><br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
    out.println(" <input type=\"submit\" name=\"update\" value=\"Update\">");
    out.println("</form>");
    out.println("<hr>");
