1712e4e700f603ba5730835836e30b71534937e7
peter <peternguyentn98@gmail.com>
2020-05-19 17:26:04 -0400

    added predicate evaluator servlet

---
+++ src/main/java/servlet/PredicateEvaluator.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "PredicateEvaluator",
+        urlPatterns = {"/evaluate"}
+    )
+
+public class PredicateEvaluator extends HttpServlet
+{
+   // Location of servlet.
+   static String Path = "swe432-servlets.herokuapp.com/rating";
+
+   /** *****************************************************
+    *  Overrides HttpServlet's doPost().
+    *  Converts the values in the form, performs the operation
+    *  indicated by the submit button, and sends the results
+    *  back to the client.
+   ********************************************************* */
+   public void doPost (HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException
+   {
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+
+      out.println("<html>"); 
+      out.println("<title>Test</title>");
+      out.println("<header>");
+      out.println("Hello World");
+      out.println("</header>");
+      out.println("</html>");
+   }  // End doPost
+
+   /** *****************************************************
+    *  Overrides HttpServlet's doGet().
+    *  Prints an HTML page with a blank form.
+   ********************************************************* */
+   public void doGet (HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException
+   {} // End doGet
+}
