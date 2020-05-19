0611f520a92ab278a0709b356a2e828a42dbd3f6
peter <peternguyentn98@gmail.com>
2020-05-19 18:49:56 -0400

    fixed bug

--- src/main/java/servlet/PredicateEvaluator.java
+++ src/main/java/servlet/PredicateEvaluator.java
@@ -57,8 +57,8 @@ public class PredicateEvaluator extends HttpServlet
          out.println("  * " + a[k]);
          out.println("</br>");
       }
-      for(int k = 0; k < c.length; k++) {
-         out.println("  * " + c[k]);
+      for(int k = 0; k < ops.length; k++) {
+         out.println("  * " + ops[k]);
          out.println("</br>");
       }
       int[] tVals = new int [a.length];
